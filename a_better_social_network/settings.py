1cf44aa0300c381dacd48027f628d8340ac3f8cd
janinquisitor <jancarlos0195@gmail.com>
2021-11-29 21:59:25 -0400

    add bookmark functionality

--- a_better_social_network/settings.py
+++ a_better_social_network/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookmarks',
-    'account'
+    'account',
+    'images'
 ]
 
 MIDDLEWARE = [
--- a_better_social_network/urls.py
+++ a_better_social_network/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('images/', include('images.urls', namespace='images')),
     path('account/', include('account.urls')),
+
 ]
