8f6fdb0d121c65ae71b8ea68b39ef4c94dfcba70
janinquisitor <jancarlos0195@gmail.com>
2021-11-30 22:10:50 -0400

    add new templates and authentication

--- a_better_social_network/settings.py
+++ a_better_social_network/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'bookmarks',
     'account',
     'images'
@@ -119,6 +120,12 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'dashboard'
--- account/urls.py
+++ account/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
+    path('edit/', views.edit, name='edit'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
